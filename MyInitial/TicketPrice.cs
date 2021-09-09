e36d1a36e3707fdec60e332a864825249b23ebbc
Nicholas Turci <nicholas.turci.personal@gmail.com>
2021-09-09 10:37:49 +0930

    Added a 'child discount' option. Note: there's an error preventing the removal of the discount.

--- MyInitial/TicketPrice.cs
+++ MyInitial/TicketPrice.cs
@@ -9,14 +9,16 @@ namespace Ticketing
     {
         private int section;
         private int quantity;
-        private bool discount;
+        private int discount;
         private decimal amountDue;
         private decimal mPrice;
 
         const decimal mdecBalcony = 35.5m;
         const decimal mdecGeneral = 28.75m;
         const decimal mdecBox = 62.0m;
-        const decimal mdecDiscount = 5.0m; 
+
+        const decimal mdecDiscount = 5.0m;
+        const decimal mdecChildDiscount = 10.0m;
 
         private int Section
         {
@@ -30,7 +32,7 @@ namespace Ticketing
             set { quantity = value; }
         }
 
-         private bool Discount
+         private int Discount
         {
             get { return discount; }
             set { discount = value; }
@@ -43,7 +45,7 @@ namespace Ticketing
         }
 
     // Constructor for TcicketPrice
-    public TicketPrice(int section, int quantity, bool discount)
+    public TicketPrice(int section, int quantity, int discount)
     {
         Section = section;
         Quantity = quantity;
@@ -66,8 +68,22 @@ namespace Ticketing
                  mPrice = mdecBox;
                  break;
          }
-         if (discount)
-         { mPrice -= mdecDiscount; }
+
+            decimal tempDiscount = 0m;
+            switch (discount)
+            {
+                case 1:
+                    tempDiscount = mdecDiscount;
+                    break;
+                case 2:
+                    tempDiscount = mdecChildDiscount;
+                    break;
+                //default:
+                //    tempDiscount = 0m;
+                //    break;
+            }
+
+         mPrice -= discount;
 
          AmountDue = mPrice * quantity;
 
